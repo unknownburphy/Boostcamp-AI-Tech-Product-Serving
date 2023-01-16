6a62b32dc494af570a3c3e3e43369b33a2b7b4e2
unknownburphy <chrisandjj1218@gmail.com>
2023-01-16 17:17:12 +0900

    test2

--- part2/04-cicd/app.py
+++ part2/04-cicd/app.py
@@ -18,7 +18,7 @@ root_password = 'password'
 
 
 def main():
-    st.title("Mask Classification Model - Deploy Test -inni")
+    st.title("Mask Classification Model - Deploy Test -inni2")
 
     with open("config.yaml") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
